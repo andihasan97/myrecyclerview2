d6edec892abea5189738bc80b03bd7c58c0b9831
andihasan97 <andihasanelfalakiy@gmail.com>
2022-09-22 08:58:13 +0700

    menambahkan efek pilih setiap item

--- app/src/main/java/com/andihasan7/myrecyclerview2/ListHeroAdapter.kt
+++ app/src/main/java/com/andihasan7/myrecyclerview2/ListHeroAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
@@ -25,6 +26,10 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
         holder.imgPhoto.setImageResource(photo)
         holder.tvName.text = name
         holder.tvDescription.text = description
+        // menambahkan efek sentuh setiap item
+        holder.itemView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, "Kamu memilih " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int = listHero.size
