f60153c6ec251d06e994737796c34aed5607b283
andihasan97 <andihasanelfalakiy@gmail.com>
2022-09-22 14:47:42 +0700

    menambah GridLayout saat orientasi Landscape

--- app/src/main/java/com/andihasan7/myrecyclerview2/MainActivity.kt
+++ app/src/main/java/com/andihasan7/myrecyclerview2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.andihasan7.myrecyclerview2
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -35,7 +37,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showRecyclerList() {
-        rvHeroes.layoutManager = LinearLayoutManager(this)
+        /*
+        menambah GridLayout hanya saat orientasi landscape
+         */
+        if (applicationContext.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        } else {
+            rvHeroes.layoutManager = LinearLayoutManager(this)
+        }
+
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
         // metode klik setiap item
